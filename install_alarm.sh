c8801bc388bbb924766e2156d6589d237538d338
Andrey Gleykh <gleykh@gmail.com>
2020-03-05 12:51:42 +0500

    Refactoring install script

--- install_alarm.sh
+++ install_alarm.sh
@@ -6,7 +6,10 @@ cd ../src || exit
 javac -verbose -d ../build Main.java
 cp main/resources/electra.ttf ~/.fonts
 cd ..
+cp alarmClock.sh ./build/alarmClock.sh
 command1='cd '`pwd`/build'; '
-command2='sh alarmClock.sh'
+command2='java Main $@'
 fullCommand='alias alarmClock="'$command1$command2'"'
 echo $fullCommand >> ~/.bashrc
+source ~/.bashrc
+
